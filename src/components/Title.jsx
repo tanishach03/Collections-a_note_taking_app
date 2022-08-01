ac5d43b056766c5f305e694e2b5ad29d6ddc761a
tanishach03 <tanctanc03@gmail.com>
2022-08-01 22:50:59 +0530

    deployed the react app on github pages

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Title(){
+    return <div className="title">
+        <h1 className="main-title">Collections</h1>
+        <p>A better way to organise your thoughts.</p>
+    </div>
+}
+
+export default Title;
