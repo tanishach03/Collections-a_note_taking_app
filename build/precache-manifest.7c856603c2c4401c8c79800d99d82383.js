1645c9576e725d1d99d866bb68db8a74df0e5b3b
tanishach03 <tanctanc03@gmail.com>
2022-08-01 23:05:24 +0530

    deployed the react app on github pages

---
+++ build/precache-manifest.7c856603c2c4401c8c79800d99d82383.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a893e67ae0a0816b2457bc7d9a66f76",
+    "url": "/Collections-a_note_taking_app/index.html"
+  },
+  {
+    "revision": "33ad2cf6984f2abce024",
+    "url": "/Collections-a_note_taking_app/static/js/2.79aacc08.chunk.js"
+  },
+  {
+    "revision": "1b80ad6eda1407fd1b60",
+    "url": "/Collections-a_note_taking_app/static/js/main.c1feff47.chunk.js"
+  },
+  {
+    "revision": "ee3a67fe2c3038da13a6",
+    "url": "/Collections-a_note_taking_app/static/js/runtime-main.9e0d3a5e.js"
+  }
+]);
